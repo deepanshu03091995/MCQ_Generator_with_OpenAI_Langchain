8e28ca7f83778aa75be6ea6d143a6c4611c9f450
Deepanshu03091995 <43910264+deepanshu03091995@users.noreply.github.com>
2023-12-16 19:22:30 +0530

    update

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages,setup
 
 setup(
-    name='mcqgenrator',
+    name='mcqgenerator',
     version='0.0.1',
     author='Deepanshu Rajput',
     author_email='deepanshu.dlri@gmail.com',
